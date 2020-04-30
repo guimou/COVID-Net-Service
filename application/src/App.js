152d2b8605ae592bd9a50778034cf65ad3694f87
Guillaume Moutier <guillaume.moutier@gmail.com>
2020-04-29 22:06:20 -0400

    cell 4 was not declared

--- application/src/App.js
+++ application/src/App.js
@@ -152,6 +152,7 @@ class App extends Component {
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
 
     // Add some text to the new cells:
     cell1.innerHTML = image_name.substring(37);
